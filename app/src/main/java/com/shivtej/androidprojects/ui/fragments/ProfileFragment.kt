951382d3ce22573cf826c46bee2863fe1763cbba
Shivtej28 <shivtejhd@gmail.com>
2021-06-21 21:45:48 +0530

    In App rate done but not working

--- app/src/main/java/com/shivtej/androidprojects/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/shivtej/androidprojects/ui/fragments/ProfileFragment.kt
@@ -3,11 +3,17 @@ package com.shivtej.androidprojects.ui.fragments
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ShareCompat
 import androidx.fragment.app.Fragment
+import com.google.android.play.core.review.ReviewInfo
+import com.google.android.play.core.review.ReviewManager
+import com.google.android.play.core.review.ReviewManagerFactory
+import com.google.android.play.core.review.model.ReviewErrorCode
+import com.google.android.play.core.review.testing.FakeReviewManager
 import com.shivtej.androidprojects.databinding.FragmentProfileBinding
 import com.shivtej.androidprojects.ui.MainActivity
 
@@ -15,6 +21,9 @@ class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
     private lateinit var activity1: MainActivity
+    lateinit var manager: ReviewManager
+    var reviewInfo: ReviewInfo? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,22 +39,37 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.donateCardView.setOnClickListener {
+            // binding.paymentLayout.visibility = View.VISIBLE
+        }
+
+        initReviews()
+
         binding.rateUsCardView.setOnClickListener {
-            val browserIntent = Intent(
-                Intent.ACTION_VIEW, Uri.parse(
-                    "https://play.google.com/store/apps/details?id=com.shivtej.androidprojects"
-                )
-            )
-            startActivity(browserIntent)
+//            val browserIntent = Intent(
+//                Intent.ACTION_VIEW, Uri.parse(
+//                    "https://play.google.com/store/apps/details?id=com.shivtej.androidprojects"
+//                )
+//            )
+//            startActivity(browserIntent)
+            askForReview()
+
         }
 
         binding.feedbackCardView.setOnClickListener {
+            val email = arrayOf("asdevelopers1428@gmail.com")
+
             val browserIntent = Intent(
-                Intent.ACTION_VIEW, Uri.parse(
-                    "asdevelopers1428@gmail.com"
-                )
+                Intent.ACTION_SENDTO
             )
-            startActivity(browserIntent)
+            browserIntent.setType("text/plain")
+                .setData(
+                    Uri.parse(
+                        "mailto:"
+                    )
+                ).putExtra(Intent.EXTRA_EMAIL, email)
+                .setPackage("com.google.android.gm")
+            startActivity(Intent.createChooser(browserIntent, "Send Your Email"))
         }
 
         binding.shareCardView.setOnClickListener {
@@ -63,6 +87,33 @@ class ProfileFragment : Fragment() {
                 )
             )
             startActivity(browserIntent)
+        }
+    }
+
+    private fun initReviews() {
+        manager = ReviewManagerFactory.create(requireContext())
+        manager.requestReviewFlow().addOnCompleteListener { request ->
+            if (request.isSuccessful) {
+                reviewInfo = request.result
+            } else {
+                // Log error
+
+                Log.i("error", "Error ")
+            }
+        }
+    }
+
+    private fun askForReview() {
+        if (reviewInfo != null) {
+            activity?.let {
+                manager.launchReviewFlow(it, reviewInfo!!).addOnFailureListener {
+                    // Log error and continue with the flow
+                    Log.i("error", "Error ")
+                }.addOnCompleteListener { _ ->
+                    // Log success and continue with the flow
+                    Log.i("error", "Success ")
+                }
+            }
         }
     }
 }
